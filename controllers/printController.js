578ee3979fb1e37c33fb5ce537c0eca4c888c2e4
Stefan Djordjevic <ste.djordjevic@gmail.com>
2019-04-02 14:02:07 +0200

    nadam se sad je final

---
+++ controllers/printController.js
@@ -0,0 +1,30 @@
+angular.module("myApp")
+    .controller("printController", ["$scope", "$rootScope", function($scope, $rootScope) {
+
+        $scope.entryDate = "Date unknown";
+        if ($rootScope.apartmentInfo.date) {
+            $scope.entryDate = $rootScope.apartmentInfo.date.toString().split(" ");
+            $scope.entryDate = $scope.entryDate[1] + "-" + $scope.entryDate[2] + "-" + $scope.entryDate[3];
+        }
+        $scope.totalPrice = 0;
+        for (let i = 0; i < $rootScope.itemsToBeReplaced.length; i++) {
+            if ($rootScope.itemsToBeReplaced[i].quantity !== undefined) {
+                $scope.totalPrice += $rootScope.itemsToBeReplaced[i].price * $rootScope.itemsToBeReplaced[i].quantity;
+            } else {
+                $scope.totalPrice += $rootScope.itemsToBeReplaced[i].price;
+            }
+        }
+    }])
+    .directive("pdfBtn", [function ($scope) {
+
+        return {
+
+            link: ($scope, element) => {
+                
+                element.on("click", () => {
+                
+                    window.print();
+                });
+            }
+        };
+    }]);
